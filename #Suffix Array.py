335f592f926d5b31f153eff6c7c602fef6629730
Minesweeper <quangn8888@gmail.com>
2022-11-09 23:55:06 -0800

    update

---
+++ #Suffix Array.py
@@ -0,0 +1,13 @@
+#Suffix Array
+
+string = 'AAAAA'
+suffixes = []
+
+for i in range(len(string)): #List of suffixes
+    suffixes.append([string[i:], i])
+
+suffixes.sort()
+
+for suffix in suffixes:
+    print(suffix[1])
+    
